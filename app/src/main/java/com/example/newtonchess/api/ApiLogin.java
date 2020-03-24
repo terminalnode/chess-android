9714d4fdf61d3cecff79414f398f28d08230fa55
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-24 03:43:09 +0100

    add dummy method for verifying login

---
+++ app/src/main/java/com/example/newtonchess/api/ApiLogin.java
@@ -0,0 +1,8 @@
+package com.example.newtonchess.api;
+
+public class ApiLogin {
+  public static boolean verifyUsername(String username, String password) {
+    // Placeholder until real API is built
+    return username.equals("callecarlsson") && password.equals("password");
+  }
+}
