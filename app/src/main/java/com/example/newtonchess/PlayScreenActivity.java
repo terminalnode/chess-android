cabc5cf61ff97b2641eef7709765f59a0a6bb864
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-24 20:56:27 +0100

    add play screen stub

---
+++ app/src/main/java/com/example/newtonchess/PlayScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.newtonchess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayScreenActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_play_screen);
+  }
+}
