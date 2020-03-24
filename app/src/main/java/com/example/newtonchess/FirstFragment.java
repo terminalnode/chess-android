3f87c43cfea5c382d3db4c9631412ddcb7d74140
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-24 04:13:53 +0100

    move login button listener into loginscreen fragment

--- app/src/main/java/com/example/newtonchess/FirstFragment.java
+++ app/src/main/java/com/example/newtonchess/FirstFragment.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.newtonchess.api.ApiLogin;
+import com.google.android.material.snackbar.Snackbar;
 
 public class FirstFragment extends Fragment {
 
@@ -23,12 +27,57 @@ public class FirstFragment extends Fragment {
   public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
 
-    view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        NavHostFragment.findNavController(FirstFragment.this)
-            .navigate(R.id.action_FirstFragment_to_SecondFragment);
-      }
-    });
+    view.findViewById(R.id.signUpButton)
+        .setOnClickListener(v -> NavHostFragment.findNavController(FirstFragment.this)
+        .navigate(R.id.action_FirstFragment_to_SecondFragment));
+
+    // Set login button listener
+    view.findViewById(R.id.logInButton)
+        .setOnClickListener(this::loginButtonPress);
+  }
+
+  private void loginButtonPress(View view) {
+    EditText userNameTextBox = getView().findViewById(R.id.userNameEditBox);
+    EditText passwordTextBox = getView().findViewById(R.id.passwordEditBox);
+    String username = userNameTextBox.getText().toString();
+    String password = passwordTextBox.getText().toString();
+
+    // Verify that login credentials were filled in and correct.
+    if (username.isEmpty() && password.isEmpty()) {
+      Snackbar.make(
+          view,
+          R.string.missingUsernamePasswordSnackbar,
+          Snackbar.LENGTH_LONG
+      ).show();
+
+    } else if (username.isEmpty()) {
+      Snackbar.make(
+          view,
+          R.string.missingUsernameSnackbar,
+          Snackbar.LENGTH_LONG
+      ).show();
+
+    } else if (password.isEmpty()) {
+      Snackbar.make(
+          view,
+          R.string.missingPasswordSnackbar,
+          Snackbar.LENGTH_LONG
+      ).show();
+
+    } else if (!ApiLogin.verifyUsername(username, password)) {
+      Snackbar.make(
+          view,
+          R.string.incorrectLoginCredentialsSnackbar,
+          Snackbar.LENGTH_LONG
+      ).show();
+
+    } else {
+      // TODO Replace this with something useful, such as switching to the main menu
+      Snackbar.make(
+          view,
+          "Login successful placeholder!!!",
+          Snackbar.LENGTH_LONG
+      ).show();
+    }
   }
 }
--- app/src/main/java/com/example/newtonchess/LoginScreenActivity.java
+++ app/src/main/java/com/example/newtonchess/LoginScreenActivity.java
@@ -1,13 +1,8 @@
 package com.example.newtonchess;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.newtonchess.api.ApiLogin;
-import com.google.android.material.snackbar.Snackbar;
 
 public class LoginScreenActivity extends AppCompatActivity {
 
@@ -15,49 +10,5 @@ public class LoginScreenActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login_screen);
-  }
-
-  public void loginButtonPress(View view) {
-    EditText userNameTextBox = findViewById(R.id.userNameEditBox);
-    EditText passwordTextBox = findViewById(R.id.passwordEditBox);
-    String username = userNameTextBox.getText().toString();
-    String password = passwordTextBox.getText().toString();
-
-    // Verify that login credentials were filled in and correct.
-    if (username.isEmpty() && password.isEmpty()) {
-      Snackbar.make(
-          view,
-          R.string.missingUsernamePasswordSnackbar,
-          Snackbar.LENGTH_LONG
-      ).show();
-
-    } else if (username.isEmpty()) {
-      Snackbar.make(
-          view,
-          R.string.missingUsernameSnackbar,
-          Snackbar.LENGTH_LONG
-      ).show();
-
-    } else if (password.isEmpty()) {
-      Snackbar.make(
-          view,
-          R.string.missingPasswordSnackbar,
-          Snackbar.LENGTH_LONG
-      ).show();
-
-    } else if (!ApiLogin.verifyUsername(username, password)) {
-      Snackbar.make(
-          view,
-          R.string.incorrectLoginCredentialsSnackbar,
-          Snackbar.LENGTH_LONG
-      ).show();
-
-    } else {
-      Snackbar.make(
-          view,
-          "Login successful placeholder!!!",
-          Snackbar.LENGTH_LONG
-      ).show();
-    }
   }
 }
