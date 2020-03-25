122de2f48d49e45d8f41e84f1bbd0977147586ea
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-25 17:24:56 +0100

    split userdata and friend, create getters/setters

--- app/src/main/java/com/example/newtonchess/api/UserData.java
+++ app/src/main/java/com/example/newtonchess/api/UserData.java
@@ -7,29 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserData implements Parcelable {
-
-
-
   private String userName;
-
   private List<Friend> friendList = new ArrayList<>();
 
-  //private List<Game> activeGames;
-
   //----- Constructors -----//
-
   public UserData(String userName) {
-
     this.userName = userName;
-
   }
 
   //----- Methods -----//
-
   public void addFriend(String friendName, int friendId) {
-
     friendList.add(new Friend(friendName, friendId));
-
   }
 
   @Override
@@ -48,9 +36,7 @@ public class UserData implements Parcelable {
     in.readTypedList(friendList, Friend.CREATOR);
   }
 
-
   //----- Getters and Setters -----//
-
   public void setUserName(String userName) {
     this.userName = userName;
   }
@@ -59,9 +45,7 @@ public class UserData implements Parcelable {
     return userName;
   }
 
-
   //----- Creator -----//
-
   public static final Creator<UserData> CREATOR = new Creator<UserData>() {
     @Override
     public UserData createFromParcel(Parcel in) {
@@ -73,49 +57,5 @@ public class UserData implements Parcelable {
       return new UserData[size];
     }
   };
-
 }
 
-class Friend implements Parcelable {
-
-  int userId;
-  int gameId;
-
-  String name;
-
-  public Friend(String name, int id) {
-    this.name = name;
-    this.userId = id;
-  }
-
-
-  protected Friend(Parcel in) {
-    userId = in.readInt();
-    gameId = in.readInt();
-    name = in.readString();
-  }
-
-  public static final Creator<Friend> CREATOR = new Creator<Friend>() {
-    @Override
-    public Friend createFromParcel(Parcel in) {
-      return new Friend(in);
-    }
-
-    @Override
-    public Friend[] newArray(int size) {
-      return new Friend[size];
-    }
-  };
-
-  @Override
-  public int describeContents() {
-    return 0;
-  }
-
-  @Override
-  public void writeToParcel(Parcel dest, int flags) {
-    dest.writeInt(userId);
-    dest.writeInt(gameId);
-    dest.writeString(name);
-  }
-}
