4cab84a05b592a4068f5e7dd90b2b795892d9a97
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-30 23:05:11 +0200

    improve signup feature

--- app/src/main/java/com/example/newtonchess/LoginScreenCredentialsFragment.java
+++ app/src/main/java/com/example/newtonchess/LoginScreenCredentialsFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
@@ -16,13 +17,19 @@ import com.example.newtonchess.api.UserData;
 import com.example.newtonchess.api.entities.PlayerEntity;
 import com.google.android.material.snackbar.Snackbar;
 
-import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.internal.EverythingIsNonNull;
 
 public class LoginScreenCredentialsFragment extends Fragment {
+  private Button signInButton, signUpButton;
+  private EditText userNameTextBox, passwordTextBox;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -32,18 +39,17 @@ public class LoginScreenCredentialsFragment extends Fragment {
 
   public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    signInButton = view.findViewById(R.id.logInButton);
+    signUpButton = view.findViewById(R.id.signUpButton);
+    userNameTextBox = view.findViewById(R.id.userNameEditBox);
+    passwordTextBox = view.findViewById(R.id.passwordEditBox);
 
     // Set up listeners
-    view.findViewById(R.id.signUpButton)
-        .setOnClickListener(this::signUpButtonPress);
-
-    view.findViewById(R.id.logInButton)
-        .setOnClickListener(this::loginButtonPress);
+    signInButton.setOnClickListener(this::loginButtonPress);
+    signUpButton.setOnClickListener(this::signUpButtonPress);
   }
 
   private void loginButtonPress(View view) {
-    EditText userNameTextBox = getView().findViewById(R.id.userNameEditBox);
-    EditText passwordTextBox = getView().findViewById(R.id.passwordEditBox);
     String username = userNameTextBox.getText().toString();
     String password = passwordTextBox.getText().toString();
 
@@ -94,40 +100,100 @@ public class LoginScreenCredentialsFragment extends Fragment {
   }
 
   private void signUpButtonPress(View view) {
-    // Find the text boxes
-    EditText userNameTextBox = getView().findViewById(R.id.userNameEditBox);
-    EditText passwordTextBox = getView().findViewById(R.id.passwordEditBox);
+    // Disable the sign up button to avoid double clicking
+    // They will be reenabled once we get a response.
+    disableButtons();
 
     // Create the new user
     PlayerEntity newPlayer = new PlayerEntity();
-    newPlayer.setName(userNameTextBox.getText().toString());
-    newPlayer.setPassword(passwordTextBox.getText().toString());
-    newPlayer.setId(0);
-
-    System.out.println(newPlayer.getName());
-    System.out.println(newPlayer.getPassword());
-    System.out.println(newPlayer.getId());
+    newPlayer.setName(userNameTextBox.getText().toString().trim());
+    newPlayer.setPassword(passwordTextBox.getText().toString().trim());
 
     // Send the new user to the server
     Call<PlayerEntity> call = ApiPlayer.createPlayer(newPlayer);
     call.enqueue(new Callback<PlayerEntity>() {
       @Override
+      @EverythingIsNonNull
       public void onResponse(Call<PlayerEntity> call, Response<PlayerEntity> response) {
-        Snackbar.make(
-            view,
-            "Operation completed with status: " + response.code(),
-            Snackbar.LENGTH_LONG
-        ).show();
+        if (response.isSuccessful() && response.body() != null) {
+          accountCreatedSnackbar(view, response.body());
+        } else {
+          accountNotCreatedSnackbar(view, response);
+        }
+        enableButtons();
       }
 
       @Override
+      @EverythingIsNonNull
       public void onFailure(Call<PlayerEntity> call, Throwable t) {
-        Snackbar.make(
-            view,
-            "Generic error placeholder!!",
-            Snackbar.LENGTH_LONG
-        ).show();
+        accountCreationGenericError(view);
+        enableButtons();
       }
     });
   }
+
+  private void accountCreatedSnackbar(View view, PlayerEntity player) {
+    Snackbar.make(
+        view,
+        getString(R.string.accountCreationSuccessful, player.getName()),
+        Snackbar.LENGTH_LONG
+    ).show();
+  }
+
+  private void accountNotCreatedSnackbar(View view, Response response) {
+    String internalName = null;
+    int error = R.string.accountCreationGenericError;
+
+    // Try to read the message body to identify the error
+    try {
+      JSONObject responseBody = new JSONObject(response.errorBody().string());
+      internalName = responseBody.getString("internalName");
+    } catch (IOException | JSONException | NullPointerException ignored) { }
+
+    System.out.println(internalName);
+    // Set the error message in accord with internalName
+    if (internalName != null) {
+      switch(internalName) {
+        case "PlayerCreateMissingFieldsException":
+          error = R.string.accountCreationMissingFields;
+          break;
+
+        case "PlayerCreateUsernameTaken":
+          error = R.string.accountCreationUsernameTaken;
+          break;
+
+        default: break;
+      }
+    }
+
+    error = R.string.accountCreationGenericError;
+    Snackbar.make(
+        view,
+        error,
+        Snackbar.LENGTH_LONG
+    ).show();
+  }
+
+  private void accountCreationGenericError(View view) {
+    Snackbar.make(
+        view,
+        R.string.accountCreationGenericError,
+        Snackbar.LENGTH_LONG
+    ).show();
+  }
+
+  private void disableButtons() {
+    signUpButton.setClickable(false);
+    signInButton.setClickable(false);
+    signUpButton.setAlpha(0.5F);
+    signInButton.setAlpha(0.5F);
+  }
+
+  private void enableButtons() {
+    signUpButton.setClickable(true);
+    signInButton.setClickable(true);
+    signUpButton.setAlpha(1F);
+    signInButton.setAlpha(1F);
+  }
 }
+
