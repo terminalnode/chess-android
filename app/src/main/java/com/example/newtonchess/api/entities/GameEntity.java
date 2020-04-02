59a1030cca75ffb042e66602efa4eeaf8697b3b3
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-04-02 22:52:31 +0200

    update GameEntity with more serialized fields

--- app/src/main/java/com/example/newtonchess/api/entities/GameEntity.java
+++ app/src/main/java/com/example/newtonchess/api/entities/GameEntity.java
@@ -4,7 +4,26 @@ import com.google.gson.annotations.SerializedName;
 
 public class GameEntity {
   @SerializedName("id")
-  long id;
+  private long id;
+
+  @SerializedName("whitePlayer")
+  private PlayerEntity whitePlayer;
+
+  @SerializedName("blackPlayer")
+  private PlayerEntity blackPlayer;
+
+  @SerializedName("whitesTurn")
+  private boolean whitesTurn;
+
+  @SerializedName("turnsTaken")
+  private long turnsTaken;
+
+  @SerializedName("finished")
+  private boolean finished;
+
+  //No good piece entity yet
+  //@SerializedName("pieces")
+  //List<Piece> pieces;
 
   //----- Constructors -----//
   public GameEntity() {
@@ -16,9 +35,48 @@ public class GameEntity {
     this.id = id;
   }
 
+  public void setWhitePlayer(PlayerEntity whitePlayer) {
+    this.whitePlayer = whitePlayer;
+  }
+
+  public void setBlackPlayer(PlayerEntity blackPlayer) {
+    this.blackPlayer = blackPlayer;
+  }
+
+  public void setWhitesTurn(boolean whitesTurn) {
+    this.whitesTurn = whitesTurn;
+  }
+
+  public void setTurnsTaken(long turnsTaken) {
+    this.turnsTaken = turnsTaken;
+  }
+
+  public void setFinished(boolean finished) {
+    this.finished = finished;
+  }
 
   //----- Getters -----//
   public long getId() {
     return id;
+  }
+
+  public PlayerEntity getWhitePlayer() {
+    return whitePlayer;
+  }
+
+  public PlayerEntity getBlackPlayer() {
+    return blackPlayer;
+  }
+
+  public boolean isWhitesTurn() {
+    return whitesTurn;
+  }
+
+  public long getTurnsTaken() {
+    return turnsTaken;
+  }
+
+  public boolean isFinished() {
+    return finished;
   }
 }
