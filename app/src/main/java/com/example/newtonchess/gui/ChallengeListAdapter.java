8cd9f6fe84dbe9a03e06899b5ffcffd109f80754
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-04-02 16:14:32 +0200

    add stubs for Challenge and Game ListAdapters

---
+++ app/src/main/java/com/example/newtonchess/gui/ChallengeListAdapter.java
@@ -0,0 +1,38 @@
+package com.example.newtonchess.gui;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.newtonchess.api.entities.ChallengeEntity;
+import com.example.newtonchess.api.entities.TokenEntity;
+
+import java.util.List;
+
+public class ChallengeListAdapter extends ArrayAdapter<ChallengeEntity> {
+  private Context context;
+  private int listLayout;
+  private TokenEntity token;
+
+  public ChallengeListAdapter(
+      @NonNull Context context,
+      int listLayout,
+      @NonNull List<ChallengeEntity> objects,
+      TokenEntity token) {
+
+    super(context, listLayout, objects);
+    this.context = context;
+    this.listLayout = listLayout;
+    this.token = token;
+  }
+
+  @NonNull
+  @Override
+  public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    return convertView;
+  }
+}
