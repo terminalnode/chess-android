8ae19646aa1cb3116485f753ec910768a8d612f5
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-04-02 12:27:51 +0200

    redesign main menu

--- app/src/main/java/com/example/newtonchess/activities/MainMenuActivity.java
+++ app/src/main/java/com/example/newtonchess/activities/MainMenuActivity.java
@@ -17,6 +17,7 @@ import com.example.newtonchess.api.entities.TokenEntity;
 public class MainMenuActivity extends AppCompatActivity {
   TokenEntity token;
   Button activeGamesButton, newGameButton, friendsListButton, logoutButton;
+  TextView userNameTextView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +25,21 @@ public class MainMenuActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main_menu);
 
     // Set buttons
-    activeGamesButton = findViewById(R.id.activeGamesButton);
+    activeGamesButton = findViewById(R.id.playButton);
     newGameButton = findViewById(R.id.newGameButton);
     friendsListButton = findViewById(R.id.friendsListButton);
     logoutButton = findViewById(R.id.logoutButton);
+    userNameTextView = findViewById(R.id.userNameTextView);
 
     // Extract token from intent
     token = getIntent().getParcelableExtra("TokenEntity");
     Log.i("ACTIVITY", String.format("Main menu started, token: %s", token));
 
     // Set username in UI
-    TextView welcomeTextView = findViewById(R.id.welcomeTextView);
     PlayerEntity player = token.getPlayer();
-    String welcomeTextString = player == null ?
-        getString(R.string.mainmenuWelcome, getString(R.string.userUnknown)) :
-        getString(R.string.mainmenuWelcome, player.getName());
-    welcomeTextView.setText(welcomeTextString);
+    String welcomeTextString =
+        player == null ? "anonymous" : player.getName();
+    userNameTextView.setText(welcomeTextString);
 
     // Set button listeners
     activeGamesButton.setOnClickListener(this::activeGamesButtonPress);
